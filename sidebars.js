b7ae084e4f7d9640bbc611cc85b386e602cc2631
roc <roc@imroc.cc>
2023-10-08 17:55:42 +0800

    update at 2023-10-08 17:55:42

--- sidebars.js
+++ sidebars.js
@@ -94,6 +94,7 @@ const sidebars = {
         'envoyfilter/preserve-case',
         'envoyfilter/enable-gzip',
         'envoyfilter/limit-request-size',
+        'envoyfilter/label-traffic',
       ],
     },
     {
