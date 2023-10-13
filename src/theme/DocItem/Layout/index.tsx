b24cf5624d834bf2544be8009c94857f0b0970e8
roc <roc@imroc.cc>
2023-10-13 17:40:27 +0800

    update at 2023-10-13 17:40:27

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -14,7 +14,7 @@ import Unlisted from '@theme/Unlisted';
 import type { Props } from '@theme/DocItem/Layout';
 
 import styles from './styles.module.css';
-import Comment from '../../../components/comment';
+import Comment from '../../../components/Comment';
 
 /**
  * Decide if the toc should be rendered, on mobile or desktop viewports
