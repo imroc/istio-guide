1db70174eba10bce690621bfd0eed9a005e8023d
roc <roc@imroc.cc>
2023-10-14 15:47:55 +0800

    update at 2023-10-14 15:47:55

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,6 +95,9 @@ const config = {
         apiKey: '5d043edc32ada7f31ef29395f39ee554',
         indexName: 'imroc-istio',
         contextualSearch: false,
+        searchParameters: {
+          facetFilters: [],
+        },
       },
       // giscus 评论功能
       giscus: {
