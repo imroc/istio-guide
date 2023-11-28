02a469806b38283067affe147b02e5d8224110f0
roc <roc@imroc.cc>
2023-11-28 16:51:44 +0800

    update at 2023-11-28 16:51:44

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,8 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
+import PrismDark from './src/utils/prismDark';
+import type { Config } from '@docusaurus/types';
 const beian = '蜀ICP备2021009081号-1'
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
+const config: Config = {
   title: 'istio 实践指南', // 网站标题
   tagline: '云原生老司机带你飞', // slogan
   favicon: 'img/logo.svg', // 电子书 favicon 文件，注意替换
@@ -64,7 +59,7 @@ const config = {
         // 文档的路由前缀
         routeBasePath: '/',
         // 左侧导航栏的配置
-        sidebarPath: require.resolve('./content/sidebars.js'),
+        sidebarPath: require.resolve('./content/sidebars.ts'),
         // 每个文档左下角 "编辑此页" 的链接
         editUrl: ({ docPath }) =>
           `https://github.com/imroc/istio-guide/edit/main/content/${docPath}`,
@@ -148,7 +143,7 @@ const config = {
       },
       // 自定义代码高亮
       prism: {
-        theme: require('prism-react-renderer/themes/vsDark'),
+        theme: PrismDark,
         magicComments: [
           {
             className: 'code-block-highlighted-line',
@@ -177,9 +172,11 @@ const config = {
           'java',
           'json',
           'hcl',
+          'bash',
+          'diff',
         ],
       },
     }),
 };
 
-module.exports = config;
+export default config;
